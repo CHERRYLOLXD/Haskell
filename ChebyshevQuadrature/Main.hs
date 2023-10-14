1ccac2eede000fa944a8d505e3c224adc119d960
CHERRY <ty_1_ta@mail.ru>
2023-10-14 18:07:11 +0700

    Fix

--- ChebyshevQuadrature/Main.hs
+++ ChebyshevQuadrature/Main.hs
@@ -1,28 +1,22 @@
 import Control.Monad
 
-chebyshevQuadrature :: (Double -> Double) -> Double -> Double -> Int -> Double
-chebyshevQuadrature integralFunction lowerBound upperBound numberOfSegments =
-    sum [weight segmentIndex * integralFunction (node segmentIndex) | segmentIndex <- [0..numberOfSegments]] * (upperBound - lowerBound)
-    where
-        weight :: Int -> Double
-        weight segmentIndex
-            | segmentIndex == 0 || segmentIndex == numberOfSegments = 1.0 / (2 * fromIntegral numberOfSegments)
-            | otherwise = 1.0 / fromIntegral numberOfSegments
-
-        node :: Int -> Double
-        node segmentIndex =
-            0.5 * (upperBound - lowerBound) * cos (pi * fromIntegral (2 * segmentIndex + 1) / (2 * fromIntegral (numberOfSegments + 1)) + (lowerBound + upperBound) / 2)
+rectangleMethod :: (Double -> Double) -> Double -> Double -> Int -> Double
+rectangleMethod integralFunction lowerLimit upperLimit segmentsCount =
+    let segmentWidth = (upperLimit - lowerLimit) / fromIntegral segmentsCount
+        midpoints = [lowerLimit + segmentWidth * (fromIntegral i + 0.5) | i <- [0..segmentsCount - 1]]
+        integralSum = sum [integralFunction midpoint | midpoint <- midpoints]
+    in segmentWidth * integralSum
 
 main :: IO ()
 main =
     forever $ do
         putStrLn "Enter lower bound:"
-        lowerBound  <- readLn
+        lowerLimit  <- readLn
         putStrLn "Enter upper bound:"
-        upperBound  <- readLn
+        upperLimit  <- readLn
         putStrLn "Enter number of segments:"
-        numberOfSegments <- readLn
-        putStrLn $ "Result: " ++ show (chebyshevQuadrature integralFunction lowerBound upperBound numberOfSegments)
+        segmentsCount <- readLn
+        putStrLn $ "Result: " ++ show (rectangleMethod integralFunction lowerLimit upperLimit segmentsCount)
         where
             integralFunction :: Double -> Double
             integralFunction x =
